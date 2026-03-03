bfe2836d3a3f4aaca8ccedab6ff97067c7d22171
Nesticojavier <nestor06sep@gmail.com>
2026-03-02 21:28:41 -0400

    add BreedService class for fetching breeds and random facts

---
+++ lib/services/breed_service.dart
@@ -0,0 +1,42 @@
+import 'package:cat_directory_app/models/breed_model.dart';
+import 'package:dio/dio.dart';
+
+class BreedService {
+  final Dio dio;
+
+  BreedService(this.dio);
+
+  Future<List<BreedModel>> fetchBreeds(int page) async {
+    try {
+      final response = await dio.get(
+        '/breeds',
+        queryParameters: {'page': page},
+      );
+
+      final List data = response.data['data'];
+
+      return data.map((e) => BreedModel.fromJson(e)).toList();
+    } on DioException catch (e) {
+      throw Exception(_mapDioError(e));
+    }
+  }
+
+  Future<String> fetchRandomFact() async {
+    try {
+      final response = await dio.get('/fact');
+      return response.data['fact'];
+    } on DioException catch (e) {
+      throw Exception(_mapDioError(e));
+    }
+  }
+
+  String _mapDioError(DioException e) {
+    if (e.type == DioExceptionType.connectionTimeout) {
+      return "Tiempo de conexión agotado";
+    }
+    if (e.type == DioExceptionType.connectionError) {
+      return "Sin conexión a internet";
+    }
+    return "Error inesperado";
+  }
+}
